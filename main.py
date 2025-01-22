bfa688320fbd9ddd67ebf47be1369fbd5918468c
Daniel Campos <danielcampos14@hotmail.com.br>
2025-01-22 08:57:37 -0300

    feat: Add search to main

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
+import argparse
+from GoogleNews import GoogleNews
+
 from src.pipeline import PipelineBuilder, JoinerTypes, SamplerTypes, SolverTypes
 
+
+def config_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--search",
+        type=str,
+        required=True,
+        help="What to search for in Google News",
+    )
+    return parser
+
+
+def search(search_str: str) -> list[dict]:
+    googlenews = GoogleNews(lang="pt", region="BR", period="1d", encode="utf-8")
+    print("Google News API version:", googlenews.getVersion())
+    googlenews.enableException(True)
+    googlenews.clear()
+    googlenews.get_news(search_str)
+    results = [
+        {"title": result["title"], "media": result["media"]}
+        for result in googlenews.results()
+    ]
+    return results
+
+
 if __name__ == "__main__":
+    args = config_argparser().parse_args()
+    results = search(args.search)
+
     sampler = SamplerTypes.RANDOM
     joiner = JoinerTypes.WITH_SOURCE
     solver = SolverTypes.AS_IS
     pipe = PipelineBuilder.build(sampler, joiner, solver)
 
-    headlines = [
-        {"title": "Cruzeiro perde para o São Paulo na Copinha", "media": "G1"},
-        {
-            "title": "Cruzeiro e Atlético empatam sem gols nos EUA",
-            "media": "ESPN",
-        },
-        {
-            "title": "Gabigol comenta a sua estréia pelo Cruzeiro: Satisfeito",
-            "media": "Estado de Minas",
-        },
-        {
-            "title": "Matheus Pereira, do Cruzeiro, pede aumento salarial",
-            "media": "Itatiaia",
-        },
-        {
-            "title": "Cruzeiro ainda busca reforços nessa janela de transferência",
-            "media": "O Tempo",
-        },
-    ]
-
-    print(pipe.run(headlines))
+    print(pipe.run(results))
