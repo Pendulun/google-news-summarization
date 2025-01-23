2d445cbd990c9aaa6f641338c087057fa9a1e698
Daniel Campos <danielcampos14@hotmail.com.br>
2025-01-23 13:43:08 -0300

    refactor: Move method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
-from GoogleNews import GoogleNews
 
 from src.pipeline import PipelineBuilder, JoinerTypes, SamplerTypes, SolverTypes
+from src.search import search
 
 
 def config_argparser() -> argparse.ArgumentParser:
@@ -14,19 +14,6 @@ def config_argparser() -> argparse.ArgumentParser:
         help="What to search for in Google News",
     )
     return parser
-
-
-def search(search_str: str) -> list[dict]:
-    googlenews = GoogleNews(lang="pt", region="BR", period="1d", encode="utf-8")
-    print("Google News API version:", googlenews.getVersion())
-    googlenews.enableException(True)
-    googlenews.clear()
-    googlenews.get_news(search_str)
-    results = [
-        {"title": result["title"], "media": result["media"]}
-        for result in googlenews.results()
-    ]
-    return results
 
 
 def summarize(
---
+++ src/search.py
@@ -0,0 +1,18 @@
+from GoogleNews import GoogleNews
+
+
+def search(search_str: str) -> list[dict]:
+    """
+    Search the search_str on google news. Returns a list of dicts
+    with the news title and link.
+    """
+    googlenews = GoogleNews(lang="pt", region="BR", period="1d", encode="utf-8")
+    print("Google News API version:", googlenews.getVersion())
+    googlenews.enableException(True)
+    googlenews.clear()
+    googlenews.get_news(search_str)
+    results = [
+        {"title": result["title"], "media": result["media"]}
+        for result in googlenews.results()
+    ]
+    return results
